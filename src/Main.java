0795ab548c942623001fcfd2c0fa44f1b28b4d13
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-18 16:00:12 +0800

    iterator pattern

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,51 @@
-class Interpreter {
+// клиент для проверки кода
+class IteratorMain {
     public static void main(String[] args) {
-        Parser parser = new Parser();
-        Expression expression = parser.evaluate("-25+43-34");
-        System.out.println(expression.interpret());
-    }
-}
-
-interface Expression{
-    int interpret();
-}
-//Создаем терминальное выражение для чисел
-class NumberExpression implements Expression{
-    private int number;
-    public NumberExpression(int number) {// конструктор
-        this.number = number;
-    }
-    @Override
-    public int interpret() {// возвращает число
-        return number;
-    }
-}
-// Создаем нетерминальный класс, описывающий синтаксическое правило сложения чисел
-class AdditionExpression implements Expression {
-    private Expression leftOperand;// число слева от знака "+" в строке
-    private Expression rightOperand;// число справа от знака "+" в строке
-
-    public AdditionExpression(Expression leftOperand, Expression rightOperand) {// конструктор
-        this.leftOperand = leftOperand;
-        this.rightOperand = rightOperand;
-    }
-
-    @Override
-    public int interpret(){// возвращает сумму двух чисел по бокам от знака "+" в строке
-        return leftOperand.interpret() + rightOperand.interpret();
-    }
-}
-class SubstractionExpression implements Expression{
-    private Expression leftOperand;// число слева от знака "-" в строке
-    private Expression rightOperand;// число справа от знака "-" в строке
-
-    public SubstractionExpression(Expression leftOperand, Expression rightOperand) {// конструктор
-        this.leftOperand = leftOperand;
-        this.rightOperand = rightOperand;
-    }
-
-    @Override
-    public int interpret(){// возвращает сумму двух чисел по бокам от знака "-" в строке
-        return leftOperand.interpret() - rightOperand.interpret();
-    }
-}
-// Служебный класс для обхода абстрактного дерева рекурсивным методом
-// Конкретная реализация зависит от сложности и условия задачи
-class Parser{
-    Expression evaluate(String str){
-        int position = str.length() - 1;
-        while (position > 0){
-            if (Character.isDigit(str.charAt(position))){
-                position--;
-            } else {
-                Expression leftOperand = evaluate(str.substring(0, position));
-                Expression rightOperand = new NumberExpression(
-                        Integer.parseInt(str.substring(position + 1)));
-                char operator = str.charAt(position);
-                switch (operator) {
-                    case '+': return new AdditionExpression(leftOperand, rightOperand);
-                    case '-': return new SubstractionExpression(leftOperand, rightOperand);
-                }
-            }
+        ConcreteAggregate ca = new ConcreteAggregate();// Создаем коллекцию
+        Iterator iterator = ca.getIterator();
+        while (iterator.hasNext()){// пока в коллекции есть следующие элементы...
+            System.out.println(iterator.next());//выводим их на экран, используя метод next() итератора
         }
-        int result = Integer.parseInt(str);
-        return new NumberExpression(result);
     }
+}
+
+class ConcreteAggregate implements Aggregate{
+    private String[] patterns = {"Singleton", "Factory", "Interpreter", "Decorator", "Facade", "Prototype"};
+
+    @Override
+    public Iterator getIterator() {// переопределяем метод getIterator() интерфейса Aggregate
+        return new PatternsIterator(patterns);// вызываем конструктор итератора и передаем ссылку на массив
+    }
+}
+
+interface Iterator{
+    boolean hasNext();// вернет true, если в массиве есть не пройденные элементы
+    Object next();// возвращает следующий элемент
+}
+
+class PatternsIterator implements Iterator{// переопределяем методы интерфейса
+    private final String[] patterns;// Ссылка на массив паттернов
+    int index = 0;// Переменная index соответствует индексу элемента в массиве.
+
+    PatternsIterator(String[] patterns) {
+        this.patterns = patterns;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (index < patterns.length){
+            return true;// если index меньше длины массива - возвращает true
+        }  else {
+            return false;// иначе возвращает false, т.е. коллекция закончилась
+        }
+    }
+
+    @Override
+    public Object next() {
+        return patterns[index++];// возвращаем следующий элемент коллекции, после чего инкрементируем index
+    }
+}
+
+interface Aggregate{// описывает метод, который возвращает нам конкретный итератор
+    Iterator getIterator();
 }

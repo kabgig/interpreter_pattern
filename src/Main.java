0a6818cb81d65251bf098439e619a054d76107c1
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-18 18:42:44 +0800

    Strategy pattern 2

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,36 @@
-class StateRunner {
+class StrategyRunner {
     public static void main(String[] args) {
-        Article article = new Article();// Создаем новую статью.
-        article.publish(); //вызываем метод опубликовать
-        article.publish(); //вызываем метод опубликовать
-        article.publish(); //вызываем метод опубликовать
+        Context context = new Context();
+
+        context.setStrategy(new FirstStrategy());//назначаем контексту конкретную стратегию
+        context.executeStrategy("Запускаем стратегию");// запускам стратегию
+
+        context.setStrategy(new SecondStrategy());//назначаем контексту другую конкретную стратегию
+        context.executeStrategy("Запускаем стратегию");// запускам стратегию
     }
 }
 
-interface State{
-    void changeState(Article article);
+interface Strategy{
+    void run(String string);// определяем метод, который будет вызывать конкретный алгоритм (стратегию)
 }
-
-class Draft implements State{
+class FirstStrategy implements Strategy{
     @Override
-    public void changeState(Article article) {
-        article.setState(new Moderation());// Устанавливает статью в состояние Модерация.
-        System.out.println("Draft State: Article waiting for moderation...");
+    public void run(String string) {
+        System.out.println("Первый вариант решения "+ string);
     }
 }
-
-class Moderation implements State{
+class SecondStrategy implements Strategy{
     @Override
-    public void changeState(Article article) {
-        article.setState(new Publication());// Устанавливает статью в состояние Публикация.
-        System.out.println("Moderation State: Publishing Article...");
+    public void run(String string) {
+        System.out.println("Второй вариант решения "+ string);
     }
 }
-
-class Publication implements State{
-    @Override
-    public void changeState(Article article) {// Не изменяет состояние статьи после публикации.
-        System.out.println("Publication State: Nothing happens...");
+class Context{
+    private Strategy strategy; // ссылка на общий интерфейс
+    void setStrategy(Strategy strategy){
+        this.strategy=strategy;// сеттер стратегии
     }
-}
-
-class Article{
-    private State state;// Ссылка на объект конкретного состояния
-
-    public void setState(State state) {
-        this.state = state;
-    }
-
-    public Article(){
-        this.state = new Draft();// При создании статьи устанавливает состояние в Черновик.
-        System.out.println("Create Article...");// Сообщение о создании статьи для наглядности.
-    }
-
-    public void publish() {
-        state.changeState(this);// В качестве аргумента принимает объект статьи.
+    void executeStrategy(String string){
+        strategy.run(string);// вызываем стратегию
     }
 }
